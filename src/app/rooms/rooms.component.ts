04583f1f3139163fc0d2c3b40beaf941f53469d0
Venkat Raja Ch <chvenkatraja.work@gmail.com>
2023-07-27 17:03:12 +0300

    RoomList created

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Rooms } from './rooms';
+import { Rooms, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -17,6 +17,45 @@ export class RoomsComponent {
     bookedRooms : 10,
     totalRooms : 15
   }
+
+  roomList : RoomList[] = [
+    {
+      roomNumber : 101,
+      roomType : 'Single',
+      roomAmenities : 'TV, AC, WiFi',
+      roomPhoto : '',
+      roomPrice : 2000,
+      roomCheckInTime : new Date('2022-10-10T10:00:00'),
+      roomCheckOutTime : new Date('2022-10-10T12:00:00')
+    },
+    {
+      roomNumber : 102,
+      roomType : 'Double',
+      roomAmenities : 'TV, AC, WiFi',
+      roomPhoto : '',
+      roomPrice : 1500,
+      roomCheckInTime : new Date('2023-10-10T10:00:00'),
+      roomCheckOutTime : new Date('2023-10-10T12:00:00')
+    },
+    {
+      roomNumber : 103,
+      roomType : 'Triple',
+      roomAmenities : 'TV, AC, WiFi',
+      roomPhoto : '',
+      roomPrice : 1000,
+      roomCheckInTime : new Date('2024-10-10T10:00:00'),
+      roomCheckOutTime : new Date('2024-10-10T12:00:00')
+    },
+    {
+      roomNumber : 104,
+      roomType : 'Single',
+      roomAmenities : 'TV, AC, WiFi',
+      roomPhoto : '',
+      roomPrice : 2000,
+      roomCheckInTime : new Date('2025-10-10T10:00:00'),
+      roomCheckOutTime : new Date('2025-10-10T12:00:00')
+    },
+  ];
 
   constructor() {}
   ngOnInit() {}
--- src/app/rooms/rooms.ts
+++ src/app/rooms/rooms.ts
@@ -3,3 +3,13 @@ export interface Rooms {
     bookedRooms : number;
     totalRooms : number;
 }
+
+export interface RoomList {
+    roomNumber : number;
+    roomType : string;
+    roomAmenities : string;
+    roomPhoto : string;
+    roomPrice : number;
+    roomCheckInTime : Date;
+    roomCheckOutTime : Date;
+}
